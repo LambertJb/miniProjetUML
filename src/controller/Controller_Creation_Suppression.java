cf60b642123aa08cd6dfbd65a49efdce2333de5e
charlottedestrait <charlotte.dest@gmail.com>
2017-12-15 11:08:41 +0100

    Controller creation suppression

--- src/controller/Controller_Creation_Suppression.java
+++ src/controller/Controller_Creation_Suppression.java
@@ -1,7 +1,24 @@
 package controller;
 
+import metier.I_Catalogue;
 import metier.Produit;
 
 public class Controller_Creation_Suppression {
 	
+	private I_Catalogue catalogue = ControllerInstance.catalogue;
+	
+	public String[] demandeSuppression() {
+		
+		return catalogue.getNomProduits();
+	}
+	
+	public boolean supprimerProduit(String nomProd) {
+		
+		return catalogue.removeProduit(nomProd);
+	}
+	
+	public boolean ajouterProduit(String nomProd, double prixUnitaireHT, int qte) {
+		
+		return catalogue.addProduit(nomProd, prixUnitaireHT, qte);
+	}
 }
