e0deb8119ce311be498eba99c83240760988e2cd
LambertJb <jblambert41@gmail.com>
2017-12-15 10:57:00 +0100

    ajout des controller

--- src/controller/Controller_Achat_Vente.java
+++ src/controller/Controller_Achat_Vente.java
@@ -2,4 +2,12 @@ package controller;
 
 public class Controller_Achat_Vente {
 
+	public boolean acheterArticle(String nomProduit, int qteAchetee) {
+		return ControllerInstance.catalogue.acheterStock(nomProduit, qteAchetee);
+	}
+
+	public boolean vendreArticle(String nomProduit, int qteVendue) {
+		return ControllerInstance.catalogue.vendreStock(nomProduit, qteVendue);
+	}
+	
 }
--- src/controller/Controller_Creation_Suppression.java
+++ src/controller/Controller_Creation_Suppression.java
@@ -1,5 +1,5 @@
 package controller;
 
 public class Controller_Creation_Suppression {
-
+	
 }
